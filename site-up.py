ee27fbf21f9f86656fb4135ad66b4b4c027920c2
alfiebrown5 <alfie.brown4b@gmail.com>
2025-05-05 19:12:43 +0100

    add main script

--- site-up.py
+++ site-up.py
@@ -1,51 +1,86 @@
 import requests
 import sys
-import os
+import argparse
 import csv
+import validators
 
-def getURLs():
+headers = {}
+
+def getURLs(file, column_header):
 #Get URLs from .csv 
-    file = file.open(sys.argv[1])
-    for line in file:
-
-    print("URLs recieved")
-    
-def getStatusCode(res):
-    res = res.split('\n')
-    statusCode = res[0]
-    
-    return statusCode
-
-def checkURL():
-
-    #Make request to site
-    """
-    curl -I \
-        -H 'User-Agent: ...' \
-    
-    """
-
-
-
-    # return res.statuscode
-
-def main():
     try:
-        print("Getting URLs from CSV file...")
-        urls = getURLs()
-        print("We are checking...")
-        for url in urls:
-            statusCode = checkURL()
-            print(f"{url}:{statusCode}")
+        with open (f"{file}","r", newline="") as f:
+            reader = csv.DictReader(f)
+            urls=[]
+            if not column_header:
+                column_header = "url"
 
-        sys.exit(0)
+            for row in reader:
+                url = row[f"{column_header}"]
+                if validators.url(url) == True:
+                    urls.append(url)
+                else:
+                    print(f"{url} is not a valid URL! Ignoring.")
+            print("URLs retrieved")
+            return urls
 
     except Exception as e:
-        print(f"An error has occured:{e}")
+        print(f"Error opening the csv file: {e}")
         sys.exit(1)
 
 
+def checkURLs(urls, timeout):
+    to_output = []
+    if not timeout:
+         timeout = 10
+    timeout = int(timeout)    
+    for url in urls:
+        try: 
+            #req to URL
+            res = requests.get(url, timeout=timeout)
+        except requests.exceptions.ConnectionError:
+                print(f"Could not connect to {url}, site appears down.")
+        except requests.exceptions.Timeout:
+                print(f"Request to {url} timed out and site appears to be down.")
+        except requests.exceptions.RequestException:
+                print(f"Site {url} appears down!")
 
+        #Check response code
+        if res:
+            print(f"Site {url} is up and returned status code: {res.status_code}")
+        else:
+            print(f"Site {url} appears up but returned status code: {res.status_code}")
+        url_code = f"{url}: {res.status_code}"
+        to_output.append(url_code)
+        
+    return to_output
+    
+
+def writeOutput(output, file_name):
+    output_file = open(file_name, 'w')
+    for line in output:
+        output_file.write(f"{line[0]}: {line[1]}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", "-f", metavar="\b", help="CSV File to pass into script")
+    parser.add_argument("--column-header", "-c", metavar="\b", help="CSV Column Header for URL values (default: url)")
+    parser.add_argument("--timeout", "-t", metavar="\b", help="Timeout in seconds (default: 10)")
+    parser.add_argument("--output", "-o", metavar="Output", help="Write output to file")
+    args=parser.parse_args()
+    
+    if args.file:
+        print("Getting URLs from CSV file...")
+        urls = getURLs(args.file, args.column_header)
+        print("We are checking...")
+        output = checkURLs(urls, args.timeout)
+
+        if args.output:
+            writeOutput(output, args.output)
+        sys.exit(0)
+
+    else:
+        print("No file specified. \nUse: python3 site-up.py -f <file.csv> [optional args]")
 
 if __name__ == "__main__":
     main()
